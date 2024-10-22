a0628076c80ae57636c357cc08e543c63101778f
Spencer Palmer <monkies989@gmail.com>
2024-10-22 13:57:15 -0600

    Got the menu created

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int run = 0;
+
+        do
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("    1. Start breathing activity");
+            Console.WriteLine("    2. Start reflection activity");
+            Console.WriteLine("    3. Start listening activity");
+            Console.WriteLine("    4.Quit");
+            Console.Write("Select a choice from the menu: ");
+            int option = int.Parse(Console.ReadLine());
+
+            if (option == 1)
+            {
+
+            }
+            else if (option == 2)
+            {
+
+            }
+            else if (option == 3)
+            {
+
+            }
+            else if (option == 4)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("You did not enter a valid option.");
+            }
+
+        }
+        while (run != 4);
     }
 }
