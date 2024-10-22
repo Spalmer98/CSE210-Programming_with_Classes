e84163005ddd028ec5b480e9bb1623891d8d88c6
Spencer Palmer <monkies989@gmail.com>
2024-10-22 14:09:37 -0600

    Created a skeleton design for activities class

--- prove/Develop05/Activity.cs
+++ prove/Develop05/Activity.cs
@@ -1,4 +1,22 @@
+using System.Diagnostics;
+
 public class Activity
 {
-    
+    private string _activityName;
+    private string _description;
+    public int _activityDuration;
+
+    public Activity(string name, string description)
+    {
+        _activityName = name;
+        _description = description;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"Welcome to {_activityName}.\n");
+        Console.WriteLine(_description);
+        Console.Write("How long, in seconds, would you like for you session? ");
+        _activityDuration = Console.Read();
+    }
 }
