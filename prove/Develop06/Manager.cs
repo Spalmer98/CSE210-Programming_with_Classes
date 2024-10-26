7fd1311f313719a6035debfc9ed65b50534555de
Spalmer98 <monkies989@gmail.com>
2024-10-26 13:43:33 -0600

    Created the files for Eternal project.

---
+++ prove/Develop06/Manager.cs
@@ -0,0 +1,52 @@
+using System.IO.Compression;
+
+public class Manager
+{
+    List<Goals> _goals = new List<Goals>();
+    int _score;
+
+    public Manager()
+    {
+
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPlayerInfo()
+    {
+
+    }
+
+    public void ListGoalNames()
+    {
+
+    }
+
+    public void ListGoalDetails()
+    {
+
+    }
+
+    public void CreateGoal()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public void SaveGoals()
+    {
+
+    }
+
+    public void LoadGoals()
+    {
+        
+    }
+}
