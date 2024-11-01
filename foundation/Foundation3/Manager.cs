7950b01e1d584aa6e6935f2504c2fb86132928c3
Spalmer98 <monkies989@gmail.com>
2024-10-31 23:28:28 -0600

    No changes

--- foundation/Foundation3/Manager.cs
+++ foundation/Foundation3/Manager.cs
@@ -5,4 +5,9 @@ public class Manager
         
     }
 
+    public void Run()
+    {
+        
+    }
+
 }
--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Manager m1 = new Manager();
+        m1.Run();
     }
 }
